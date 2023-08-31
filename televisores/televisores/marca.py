cbd181c56245ff1a3419dfe08fd8c1d4213ea1e4
Marcos-pixel-develop <73051397+Marcos-pixel-develop@users.noreply.github.com>
2023-08-31 00:19:21 -0500

    Add files via upload

---
+++ televisores/televisores/marca.py
@@ -0,0 +1,11 @@
+import control
+import tv
+class Marca:
+    def __init__(self, nombre):
+        self._nombre = nombre
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
---
+++ televisores/televisores/tv.py
@@ -0,0 +1,71 @@
+import control
+import marca
+class TV:
+    numTV = 0
+
+    def __init__(self, marca, estado):
+        self._marca = marca
+        self._canal = 1
+        self._precio = 500
+        self._estado = estado
+        self._volumen = 1
+        self._control = None
+        TV.numTV += 1
+
+    def setMarca(self, marca):
+        self._marca = marca
+
+    def getMarca(self):
+        return self._marca
+
+    def setCanal(self, canal):
+        if self._estado and 1 <= canal <= 120:
+            self._canal = canal
+
+    def getCanal(self):
+        return self._canal
+
+    def setPrecio(self, precio):
+        self._precio = precio
+
+    def getPrecio(self):
+        return self._precio
+
+    def setVolumen(self, volumen):
+        if self._estado and 0 <= volumen <= 7:
+            self._volumen = volumen
+
+    def getVolumen(self):
+        return self._volumen
+
+    def setControl(self, control):
+        self._control = control
+
+    def getControl(self):
+        return self._control
+
+    def turnOn(self):
+        self._estado = True
+
+    def turnOff(self):
+        self._estado = False
+
+    def getEstado(self):
+        return self._estado
+
+    def canalUp(self):
+        if self._estado and self._canal < 120:
+            self._canal += 1
+
+    def canalDown(self):
+        if self._estado and self._canal > 1:
+            self._canal -= 1
+
+    def volumenUp(self):
+        if self._estado and self._volumen < 7:
+            self._volumen += 1
+
+    def volumenDown(self):
+        if self._estado and self._volumen > 0:
+            self._volumen -= 1
+
